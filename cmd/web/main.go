e682c9fb3d2fb06c0302592391b07f089f36a0f5
akyrey <stein.dario@gmail.com>
2023-12-26 17:58:14 +0100

    refactor: replace log with slog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/akyrey/snippetbox/internal"
 )
@@ -11,17 +12,23 @@ func main() {
 	config := internal.Config{}
 	config.Parse()
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir(config.StaticDir))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
 
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir(config.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("starting server on %s\n", config.Addr)
+	logger.Info("starting server", slog.String("addr", config.Addr))
 
 	err := http.ListenAndServe(config.Addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
