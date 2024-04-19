06bca766a5349dc7f72a40a79948f303541baa30
akyrey <stein.dario@gmail.com>
2024-04-19 10:38:16 +0200

    feat: handle form validation on snippet create

--- internal/handlers.go
+++ internal/handlers.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/akyrey/snippetbox/internal/models"
 )
+
+// snippetCreateForm is a struct that represents the form fields for creating a new snippet.
+// All fields must be exported so that the template will be able to access them.
+type snippetCreateForm struct {
+	FieldErrors map[string]string
+	Title       string
+	Content     string
+	Expires     int
+}
 
 func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 	snippetModel := models.SnippetModel{DB: app.DB}
@@ -50,6 +61,9 @@ func (app *Application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 func (app *Application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := NewTemplateData(r)
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
 
 	app.render(w, r, http.StatusOK, "create.tmpl", &data)
 }
@@ -61,16 +75,41 @@ func (app *Application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: make(map[string]string),
+	}
+
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		// NOTE: utf8.RuneCountInString counts the number of unicode code points
+		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
+	}
+	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
+		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	if len(form.FieldErrors) > 0 {
+		data := NewTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", &data)
+		return
+	}
+
 	snippets := models.SnippetModel{DB: app.DB}
-	id, err := snippets.Insert(title, content, expires)
+	id, err := snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
--- internal/templates.go
+++ internal/templates.go
@@ -10,6 +10,7 @@ import (
 )
 
 type templateData struct {
+	Form        any
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 	CurrentYear int
